4bba1f4c174ce320f4efc047d284247220133b5c
david-ae <davidedet47@gmail.com>
2026-09-14 08:05:08 +0100

    Update Swagger for live

--- Gilead.API/Program.cs
+++ Gilead.API/Program.cs
@@ -33,11 +33,8 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors(AllowAnyCorsPolicy);
